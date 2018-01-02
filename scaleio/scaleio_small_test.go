c347ff1dafb6bbc8a9f2741f4120c31e01298c83
James King <king-jam@users.noreply.github.com>
2018-01-02 13:38:07 -0500

    Updated ScaleIO Plugin (#19)
    * Updated ScaleIO Plugin
    
    * Fixed Dynamic Namespaces not properly being created
    * Updated to use plugin lib
    * Updated example to be more current
    * Refactored Storage Pool code to be more extensible
    
    * Removed magic numbers

--- scaleio/scaleio_small_test.go
+++ scaleio/scaleio_small_test.go
@@ -24,23 +24,12 @@ package scaleio
 import (
 	"testing"
 
-	"github.com/intelsdi-x/snap/control/plugin"
+	"github.com/intelsdi-x/snap-plugin-lib-go/v1/plugin"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestPluginCreation(t *testing.T) {
-	Convey("Meta should return the right data", t, func() {
-		metadata := Meta()
-		// Check to make sure everything matches
-		So(metadata.Name, ShouldResemble, name)
-		So(metadata.Version, ShouldResemble, version)
-		So(metadata.Type, ShouldResemble, pluginType)
-		So(metadata.AcceptedContentTypes, ShouldResemble, []string{plugin.SnapGOBContentType})
-		So(metadata.Exclusive, ShouldBeTrue)
-		So(metadata.RoutingStrategy, ShouldResemble, plugin.StickyRouting)
-	})
-
 	Convey("Should return a new ScaleIO plugin instance", t, func() {
 		col := NewScaleIOCollector()
 		Convey("Plugin should not be nil", func() {
@@ -56,29 +45,9 @@ func TestPluginCreation(t *testing.T) {
 func TestConfigPolicy(t *testing.T) {
 	Convey("GetConfigPolicy should return a valid policy", t, func() {
 		s := NewScaleIOCollector()
-		policy, err := s.GetConfigPolicy()
+		_, err := s.GetConfigPolicy()
 		Convey("No errors should occur in getting the policy", func() {
 			So(err, ShouldBeNil)
-		})
-		policies := policy.GetAll()
-		Convey("Policy node should exist", func() {
-			So(policies, ShouldHaveLength, 1)
-		})
-		node := policies[0]
-		rules := node.RulesAsTable()
-		Convey("All rules should exist in policy node", func() {
-			So(node.HasRules(), ShouldBeTrue)
-			So(rules, ShouldHaveLength, 4)
-		})
-
-		Convey("Rules table should contain gateway, username, and password", func() {
-			var names []string
-			for _, v := range rules {
-				names = append(names, v.Name)
-			}
-			So(names, ShouldContain, "gateway")
-			So(names, ShouldContain, "username")
-			So(names, ShouldContain, "password")
 		})
 	})
 }
@@ -86,12 +55,13 @@ func TestConfigPolicy(t *testing.T) {
 func TestGetMetricTypes(t *testing.T) {
 	Convey("GetMetricTypes should return the correct number of metrics", t, func() {
 		s := NewScaleIOCollector()
-		metrics, err := s.GetMetricTypes(plugin.NewPluginConfigType())
+		config := plugin.Config{}
+		metrics, err := s.GetMetricTypes(config)
 		Convey("Should not return error", func() {
 			So(err, ShouldBeNil)
 		})
 		Convey("Has the correct number of metrics", func() {
-			So(metrics, ShouldHaveLength, len(metricKeys))
+			So(metrics, ShouldHaveLength, len(storagePoolMetricKeys))
 		})
 	})
 }
